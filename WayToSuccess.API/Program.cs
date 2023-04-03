b63677a823380649b52f6675cd1ba1f6af601dae
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-04-03 21:25:47 +0500

    created TopicController with TopicService

--- WayToSuccess.API/Program.cs
+++ WayToSuccess.API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<ITopicService, TopicService>();
 builder.Services.AddAutoMapper(typeof(AutoMapperConfiguration).Assembly);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
