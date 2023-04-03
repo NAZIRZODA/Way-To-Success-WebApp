673804258c06d8fcc9600e80b84b6b01e8670f9d
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-04-03 08:31:02 +0500

    TopicRequest/ResponseModel

--- WTSuccess.Application/Requests/TopicRequests/TopicRequestModel.cs
+++ WTSuccess.Application/Requests/TopicRequests/TopicRequestModel.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WTSuccess.Domain.Models;
 
 namespace WTSuccess.Application.Requests.TopicRequests
 {
     public class TopicRequestModel : BaseRequest
     {
-
+        public string Teory { get; set; }
+        public Chapter Chapter { get; set; }
+        public ulong ChapterId { get; set; }
     }
 }
--- WTSuccess.Application/Responses/TopicResponses/TopicResponseModel.cs
+++ WTSuccess.Application/Responses/TopicResponses/TopicResponseModel.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WTSuccess.Domain.Models;
 
 namespace WTSuccess.Application.Responses.TopicResponses
 {
     public class TopicResponseModel : BaseResponse
     {
+        public string Teory { get; set; }
+        public Chapter Chapter { get; set; }
+        public ulong ChapterId { get; set; }
     }
 }
