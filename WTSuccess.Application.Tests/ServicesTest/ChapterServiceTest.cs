17f67cb2db952312027d5f323cf57d2b464604ac
Jahongir Akbarov <99798111+AkbarovJahongir@users.noreply.github.com>
2023-04-11 16:53:09 +0500

    Error correction

--- WTSuccess.Application.Tests/ServicesTest/ChapterServiceTest.cs
+++ WTSuccess.Application.Tests/ServicesTest/ChapterServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Net;
 using WTSuccess.Application.Common.Interfaces.Repositories;
 using WTSuccess.Application.Exceptions;
 using WTSuccess.Application.Requests.ChapterRequests;
@@ -12,14 +13,14 @@ using Assert = NUnit.Framework.Assert;
 
 namespace WTSuccess.UnitTests.Application.Services
 {
-    [TestClass]
+    [TestFixture]
     public class ChapterServiceTests
     {
         private ChapterService _chapterService;
         private Mock<IChapterRepository> _mockChapterRepository;
         private Mock<IMapper> _mockMapper;
 
-        [TestInitialize]
+        [SetUp]
         public void Setup()
         {
             _mockChapterRepository = new Mock<IChapterRepository>();
@@ -27,124 +28,60 @@ namespace WTSuccess.UnitTests.Application.Services
             _chapterService = new ChapterService(_mockChapterRepository.Object, _mockMapper.Object);
         }
 
-        [Test]
-        public void Add_WithValidRequest_CallsRepositoryAddAndSaveChanges()
-        {
-            var request = new CreateChapterRequestModel();
-            var chapter = new Chapter();
-            _mockMapper.Setup(x => x.Map<CreateChapterRequestModel, Chapter>(request)).Returns(chapter);
 
+        [Test]
+        public void Add_ValidRequest_ShouldAddChapterToRepository()
+        {
+            // Arrange
+            var request = new CreateChapterRequestModel { Name = "Chapter 1" };
+
+            // Act
             _chapterService.Add(request);
 
-            _mockChapterRepository.Verify(x => x.Add(chapter), Times.Once);
+            // Assert
+            _mockChapterRepository.Verify(x => x.Add(It.IsAny<Chapter>()), Times.Once);
             _mockChapterRepository.Verify(x => x.SaveChanges(), Times.Once);
         }
 
         [Test]
-        [ExpectedException(typeof(HttpStatusCodeException))]
-        public void Add_WithInvalidRequest_ThrowsHttpStatusCodeException()
+        public void Add_InvalidRequest_ShouldThrowHttpStatusCodeException()
         {
-            var request = new UpdateChapterRequestModel();
+            // Arrange
+            var request = new UpdateChapterRequestModel { Name = "Chapter 1" };
 
-            _chapterService.Add(request);
+            // Act & Assert
+            var ex = Assert.Throws<HttpStatusCodeException>(() => _chapterService.Add(request));
+            Assert.AreEqual(HttpStatusCode.NotFound, ex.StatusCode);
+            Assert.AreEqual(nameof(Student), ex.Message);
         }
         [Test]
-        public void Get_ReturnsCorrectChapter()
+        public void Get_ExistingChapterId_ShouldReturnChapterResponseModel()
         {
-            var chapterId = 1;
-            var expectedChapter = new Chapter { Id = (ulong)chapterId, Name = "Chapter 1" };
-            var mockRepository = new Mock<IChapterRepository>();
-            mockRepository.Setup(repo => repo.FindById((ulong)chapterId)).Returns(expectedChapter);
-            var mockMapper = new Mock<IMapper>();
-            mockMapper.Setup(mapper => mapper.Map<Chapter, ChapterResponseModel>(It.IsAny<Chapter>())).Returns(new ChapterResponseModel());
-            var chapterService = new ChapterService(mockRepository.Object, mockMapper.Object);
-
-            var result = chapterService.Get((ulong)chapterId);
-
-            Assert.IsNotNull(result);
-            mockRepository.Verify(repo => repo.FindById((ulong)chapterId), Times.Once);
-            mockMapper.Verify(mapper => mapper.Map<Chapter, ChapterResponseModel>(It.IsAny<Chapter>()), Times.Once);
-        }
-        [Test]
-        public void GetAll_ReturnsAllChapters()
-        {
-            var mockRepository = new Mock<IChapterRepository>();
-            var mockMapper = new Mock<IMapper>();
-
-            var expectedChapters = new List<Chapter>
-            {
-                new Chapter { Id = 1, Name = "Chapter 1" },
-                new Chapter { Id = 2, Name = "Chapter 2" },
-                new Chapter { Id = 3, Name = "Chapter 3" }
-            };
-
-            mockRepository.Setup(x => x.GetAll(It.IsAny<int>(), It.IsAny<int>())).Returns((IQueryable<Chapter>)expectedChapters);
-
-            var expectedResponseModels = expectedChapters.Select(c => new ChapterResponseModel { CourseId = c.Id, Name = c.Name });
-
-            mockMapper.Setup(x => x.Map<IEnumerable<Chapter>, IEnumerable<ChapterResponseModel>>(expectedChapters))
-                .Returns(expectedResponseModels);
-
-            var service = new ChapterService(mockRepository.Object, mockMapper.Object);
-            var result = service.GetAll(10, 1);
-            Assert.NotNull(result);
-            Assert.AreEqual(expectedResponseModels.Count(), result.Count());
-            Assert.AreEqual(expectedResponseModels, result);
-        }
-        [Test]
-        public void UpdateChapter_WithValidIdAndRequest_ReturnsChapterResponseModel()
-        {
-            ulong id = 1;
-            var request = new UpdateChapterRequestModel { Name = "Updated Title"};
-            var dbChapter = new Chapter { Id = id, Name = "Original Title"};
-            var expectedResponse = new ChapterResponseModel { CourseId = id, Name = "Updated Title"};
-            var mockRepository = new Mock<IChapterRepository>();
-            var mockMapper = new Mock<IMapper>();
-            mockRepository.Setup(x => x.FindById(id)).Returns(dbChapter);
-            mockMapper.Setup(x => x.Map<UpdateChapterRequestModel, Chapter>(request, dbChapter))
-                .Returns(dbChapter);
-            mockMapper.Setup(x => x.Map<Chapter, ChapterResponseModel>(dbChapter))
-                .Returns(expectedResponse);
-            _chapterService = new ChapterService(mockRepository.Object, mockMapper.Object);
-            var response = _chapterService.Update(id, request);
-            Assert.AreEqual(expectedResponse.CourseId, response.CourseId);
-            Assert.AreEqual(expectedResponse.Name, response.Name);
-        }
-
-        [Test]
-        [ExpectedException(typeof(HttpStatusCodeException))]
-        public void UpdateChapter_WithInvalidId_ThrowsHttpStatusCodeException()
-        {
-            var id = 1;
-            var request = new UpdateChapterRequestModel { Name = "Updated Title"};
-            Chapter dbChapter = null;
-            var mockRepository = new Mock<IChapterRepository>();
-            var mockMapper = new Mock<IMapper>();
-            mockRepository.Setup(x => x.FindById((ulong)id)).Returns(dbChapter);
-            _chapterService = new ChapterService(mockRepository.Object, mockMapper.Object);
-            _chapterService.Update((ulong)id, request);
-        }
-        [Test]
-        public void Delete_ShouldReturnTrue_WhenChapterExists()
-        {
-            var chapterToDelete = new Chapter { Id = 1 };
-
-            _mockChapterRepository.Setup(x => x.FindById(1)).Returns(chapterToDelete);
-
-            var result = _chapterService.Delete(1);
-
-            Assert.IsTrue(result);
-            _mockChapterRepository.Verify(x => x.Delete(chapterToDelete), Times.Once);
-            _mockChapterRepository.Verify(x => x.SaveChanges(), Times.Once);
-        }
-
-        [Test]
-        public void Delete_ShouldThrowHttpStatusCodeException_WhenChapterDoesNotExist()
-        {
+            // Arrange
             ulong chapterId = 1;
-            _mockChapterRepository.Setup(x => x.FindById(1)).Returns((Chapter)null);
+            var chapter = new Chapter { Id = chapterId, Name = "Chapter 1" };
+            _mockChapterRepository.Setup(x => x.FindById(chapterId)).Returns(chapter);
+            var chapterService = new ChapterService(_mockChapterRepository.Object, _mockMapper.Object);
+            // Act
+            ChapterResponseModel result = chapterService.Get(chapterId);
 
-            Assert.Throws<HttpStatusCodeException>(() => _chapterService.Delete(1));
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(chapterId, result.CourseId);
+            Assert.AreEqual(chapter.Name, result.Name);
+        }
+
+        [Test]
+        public void Get_NonExistingChapterId_ShouldThrowHttpStatusCodeException()
+        {
+            // Arrange
+            ulong chapterId = 1;
+            _mockChapterRepository.Setup(x => x.FindById(chapterId)).Returns((Chapter)null);
+
+            // Act & Assert
+            var ex = Assert.Throws<HttpStatusCodeException>(() => _chapterService.Get(chapterId));
+            Assert.AreEqual(HttpStatusCode.NotFound, ex.StatusCode);
+            Assert.AreEqual(nameof(Student), ex.Message);
         }
     }
 }
