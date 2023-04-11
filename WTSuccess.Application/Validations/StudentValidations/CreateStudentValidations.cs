39b6b4b4abffd34158cae24c7f6f1e00e02c9489
Dilshod <nazirzodadilshod@gmail.com>
2023-04-11 14:18:28 +0500

    Fixed and improved CreateStudentValidations

--- WTSuccess.Application/Validations/StudentValidations/CreateStudentValidations.cs
+++ WTSuccess.Application/Validations/StudentValidations/CreateStudentValidations.cs
@@ -13,9 +13,13 @@ namespace WTSuccess.Application.Validations.StudentValidations
     {
         public CreateStudentValidations()
         {
-            RuleFor(s => s.Name).NotEmpty().NotNull();
-            RuleFor(s=>s.Surname).NotEmpty().NotNull();
-            RuleFor(s=>s.Email).EmailAddress().NotEmpty().NotNull();
+            RuleFor(s => s.Name).NotEmpty().NotNull().WithMessage("Name can not be null or empty!");
+            RuleFor(s => s.Surname).NotEmpty().NotNull().WithMessage("Surname can not be null or empty!");
+            RuleFor(s => s.Email).EmailAddress().NotEmpty().NotNull().WithMessage("Email address type is wrong!");
+            RuleFor(p => p.Password).NotEqual(s => s.Surname).WithMessage("Password can not be equal with Surname!");
+            RuleFor(p => p.Password).NotEqual(s => s.Name).WithMessage("Password can not be equal with Name!");
+            RuleFor(p => p.Password).NotEqual(e => e.Email).WithMessage("Password can not be equal with Email!");
+            RuleFor(p => p.Password).MinimumLength(8).MaximumLength(32).WithMessage("Min character is 8 max 32");
         }
     }
 }
