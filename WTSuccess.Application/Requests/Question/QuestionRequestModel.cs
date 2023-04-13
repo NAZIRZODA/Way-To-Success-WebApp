f8019c678878d3fad07c2b6031b0a117fcf75d6e
Dilshod <nazirzodadilshod@gmail.com>
2023-04-14 02:49:28 +0500

    deleted id for answer and question models

--- WTSuccess.Application/Requests/Question/QuestionRequestModel.cs
+++ WTSuccess.Application/Requests/Question/QuestionRequestModel.cs
@@ -3,7 +3,6 @@ namespace WTSuccess.Application.Requests.Question
 {
     public class QuestionRequestModel: BaseRequest
     {
-        public ulong Id { get; set; }
         public string Text { get; set; }
         public ulong ChapterId { get; set; }
     }
